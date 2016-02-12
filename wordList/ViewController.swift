ac781ba90ac09a306f27e9ea14eb8925e25dd19c
hmlab book pro <saki.tommy69@gmail.com>
2016-02-12 17:16:50 +0900

    Initial Commit

---
+++ wordList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wordList
+//
+//  Created by hmlab book pro on 2016/02/12.
+//  Copyright © 2016年 hmlab book pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
